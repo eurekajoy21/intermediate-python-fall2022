ad049489e4d17a3b584aa0a29ca9230fd4e0d5d0
Robert Ritz <robertritz@outlook.com>
2021-09-22 18:56:36 +0800

    updated pset0

---
+++ Week 02 - Writing Good Functions/.ipynb_checkpoints/my_function-checkpoint.py
@@ -0,0 +1,7 @@
+def test_function(argument):
+    print(argument)
+    argument1 = next_function(argument)
+    return argument1
+    
+def next_function(argument):
+    return argument
---
+++ Week 02 - Writing Good Functions/my_function.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def test_function(argument):
+    print(argument)
+    argument1 = next_function(argument)
+    return argument1
+    
+def next_function(argument):
+    return argument
